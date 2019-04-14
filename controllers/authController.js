3a6ea9abb98bdb161d3028fae8d66e2fc323796f
Toon Verbeek <toon.verbeek1@gmail.com>
2019-04-14 19:05:51 +0200

    remove unused rendering

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,20 @@
-const passport = require('passport');
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const promisify = require('es6-promisify');
-const mail = require('../handlers/mail');
+const passport = require("passport");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const promisify = require("es6-promisify");
+const mail = require("../handlers/mail");
 
-exports.login = passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: 'Failed Login!',
-  successRedirect: '/',
-  successFlash: 'You are now logged in!'
+exports.login = passport.authenticate("local", {
+  failureRedirect: "/login",
+  failureFlash: "Failed Login!",
+  successRedirect: "/",
+  successFlash: "You are now logged in!"
 });
 
 exports.logout = (req, res) => {
   req.logout();
-  req.flash('success', 'You are now logged out! 👋');
-  res.redirect('/');
+  res.json({ isLoggedIn: false });
 };
 
 exports.isLoggedIn = (req, res, next) => {
@@ -24,32 +23,33 @@ exports.isLoggedIn = (req, res, next) => {
     next(); // carry on! They are logged in!
     return;
   }
-  req.flash('error', 'Oops you must be logged in to do that!');
-  res.redirect('/login');
+  res.status(200).json({ isLoggedIn: false });
 };
 
 exports.forgot = async (req, res) => {
   // 1. See if a user with that email exists
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    req.flash('error', 'No account with that email exists.');
-    return res.redirect('/login');
+    req.flash("error", "No account with that email exists.");
+    return res.redirect("/login");
   }
   // 2. Set reset tokens and expiry on their account
-  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
   user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
   await user.save();
   // 3. Send them an email with the token
-  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  const resetURL = `http://${req.headers.host}/account/reset/${
+    user.resetPasswordToken
+  }`;
   await mail.send({
     user,
-    filename: 'password-reset',
-    subject: 'Password Reset',
+    filename: "password-reset",
+    subject: "Password Reset",
     resetURL
   });
-  req.flash('success', `You have been emailed a password reset link.`);
+  req.flash("success", `You have been emailed a password reset link.`);
   // 4. redirect to login page
-  res.redirect('/login');
+  res.redirect("/login");
 };
 
 exports.reset = async (req, res) => {
@@ -58,20 +58,20 @@ exports.reset = async (req, res) => {
     resetPasswordExpires: { $gt: Date.now() }
   });
   if (!user) {
-    req.flash('error', 'Password reset is invalid or has expired');
-    return res.redirect('/login');
+    req.flash("error", "Password reset is invalid or has expired");
+    return res.redirect("/login");
   }
   // if there is a user, show the rest password form
-  res.render('reset', { title: 'Reset your Password' });
+  res.render("reset", { title: "Reset your Password" });
 };
 
 exports.confirmedPasswords = (req, res, next) => {
-  if (req.body.password === req.body['password-confirm']) {
+  if (req.body.password === req.body["password-confirm"]) {
     next(); // keepit going!
     return;
   }
-  req.flash('error', 'Passwords do not match!');
-  res.redirect('back');
+  req.flash("error", "Passwords do not match!");
+  res.redirect("back");
 };
 
 exports.update = async (req, res) => {
@@ -81,8 +81,8 @@ exports.update = async (req, res) => {
   });
 
   if (!user) {
-    req.flash('error', 'Password reset is invalid or has expired');
-    return res.redirect('/login');
+    req.flash("error", "Password reset is invalid or has expired");
+    return res.redirect("/login");
   }
 
   const setPassword = promisify(user.setPassword, user);
@@ -91,6 +91,9 @@ exports.update = async (req, res) => {
   user.resetPasswordExpires = undefined;
   const updatedUser = await user.save();
   await req.login(updatedUser);
-  req.flash('success', '💃 Nice! Your password has been reset! You are now logged in!');
-  res.redirect('/');
+  req.flash(
+    "success",
+    "💃 Nice! Your password has been reset! You are now logged in!"
+  );
+  res.redirect("/");
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,35 @@
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const promisify = require('es6-promisify');
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const promisify = require("es6-promisify");
 
 exports.loginForm = (req, res) => {
-  res.render('login', { title: 'Login' });
+  res.render("login", { title: "Login" });
 };
 
 exports.registerForm = (req, res) => {
-  res.render('register', { title: 'Register' });
+  res.render("register", { title: "Register" });
 };
 
 exports.validateRegister = (req, res, next) => {
-  req.sanitizeBody('name');
-  req.checkBody('name', 'You must supply a name!').notEmpty();
-  req.checkBody('email', 'That Email is not valid!').isEmail();
-  req.sanitizeBody('email').normalizeEmail({
+  req.sanitizeBody("name");
+  req.checkBody("name", "You must supply a name!").notEmpty();
+  req.checkBody("email", "That Email is not valid!").isEmail();
+  req.sanitizeBody("email").normalizeEmail({
     gmail_remove_dots: false,
     remove_extension: false,
     gmail_remove_subaddress: false
   });
-  req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
-  req.checkBody('password-confirm', 'Confirmed Password cannot be blank!').notEmpty();
-  req.checkBody('password-confirm', 'Oops! Your passwords do not match').equals(req.body.password);
+  req.checkBody("password", "Password Cannot be Blank!").notEmpty();
+  req
+    .checkBody("password-confirm", "Confirmed Password cannot be blank!")
+    .notEmpty();
+  req
+    .checkBody("password-confirm", "Oops! Your passwords do not match")
+    .equals(req.body.password);
 
   const errors = req.validationErrors();
   if (errors) {
-    req.flash('error', errors.map(err => err.msg));
-    res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
+    res.status(500).json({ error: errors });
     return; // stop the fn from running
   }
   next(); // there were no errors!
@@ -40,7 +43,7 @@ exports.register = async (req, res, next) => {
 };
 
 exports.account = (req, res) => {
-  res.render('account', { title: 'Edit Your Account' });
+  res.render("account", { title: "Edit Your Account" });
 };
 
 exports.updateAccount = async (req, res) => {
@@ -52,8 +55,8 @@ exports.updateAccount = async (req, res) => {
   const user = await User.findOneAndUpdate(
     { _id: req.user._id },
     { $set: updates },
-    { new: true, runValidators: true, context: 'query' }
+    { new: true, runValidators: true, context: "query" }
   );
-  req.flash('success', 'Updated the profile!');
-  res.redirect('back');
+  req.flash("success", "Updated the profile!");
+  res.redirect("back");
 };
